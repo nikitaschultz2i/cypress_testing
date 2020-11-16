dc17d5cbc2bf223c86e18cdf5e6c0e29749831e6
nikitaschultz2i <nikita.schultz@2itesting.com>
2020-11-16 11:44:25 +0000

    tests to assert each programme is present

--- cypress/integration/demo/programme_page.spec.js
+++ cypress/integration/demo/programme_page.spec.js
@@ -3,12 +3,27 @@ context('Career Page', () => {
         cy.visit('https://www.thrivetribe.org.uk/programmes');
     })
 
-    it('contains all programmes', () => {
+    it('contains a weight management programme', () => {
         cy.get('div.grid-4 p.ccprogrammes').should('contain', 'Weight management')
+    })
+
+    it('contains a stop smoking programme', () => {
         cy.get('div.grid-4 p.ccprogrammes').should('contain', 'Stop smoking')
+    })
+
+    it('contains a mental wellbeing programme', () => {
         cy.get('div.grid-4 p.ccprogrammes').should('contain', 'Mental wellbeing')
+    })
+
+    it('contains a health checks programme', () => {
         cy.get('div.grid-4 p.ccprogrammes').should('contain', 'Health checks')
+    })
+
+    it('contains a workplace health programme', () => {
         cy.get('div.grid-4 p.ccprogrammes').should('contain', 'Workplace health')
+    })
+
+    it('contains a integrated services programme', () => {
         cy.get('div.grid-4 p.ccprogrammes').should('contain', 'Integrated services')
     })
 
